1ad5a717d2e20e006380a8fbc270472b57cd2941
m-a-huber <hubermarius@hotmail.com>
2025-02-11 17:18:24 +0100

    Make sklearn compatible.

--- src/drips_complex/drips_complex.py
+++ src/drips_complex/drips_complex.py
@@ -1,34 +1,39 @@
+from typing import Optional
+
 import numpy as np
 import numpy.typing as npt
 import plotly.graph_objects as gobj  # type: ignore
 from gph import ripser_parallel  # type: ignore
 from numba import jit, prange  # type: ignore
-from sklearn.base import BaseEstimator  # type: ignore
+from sklearn.base import BaseEstimator, TransformerMixin  # type: ignore
 from sklearn.metrics import pairwise_distances  # type: ignore
-from sklearn.utils.validation import check_is_fitted  # type: ignore
 from typing_extensions import Self
 
 from plotting.persistence_plotting import plot_persistences  # type: ignore
 from plotting.point_cloud_plotting import plot_point_cloud  # type: ignore
 
 
-class DripsComplex(BaseEstimator):
+class DripsComplex(TransformerMixin, BaseEstimator):
     """ Class implementing the Dowker-Rips complex associated to a point cloud
     whose elements are separated into two classes. The data points on which the
     simplicial complex is constructed are referred to as "vertices", while the
     other ones are referred to as "witnesses".
 
     Parameters:
-        metric (str, optional): The metric used to compute distance between
-            data points. Must be one of the metrics listed in
-            ``sklearn.metrics.pairwise.PAIRWISE_DISTANCE_FUNCTIONS``.
-            Defaults to `"euclidean"`.
         max_dimension (int, optional): The maximum homology dimension computed.
             Will compute all dimensions lower than or equal to this value.
             Defaults to `1`.
         max_filtration (float, optional): The Maximum value of the Drips
             filtration parameter. If `np.inf`, the entire filtration is
             computed. Defaults to `np.inf`.
+        coeff (int, optional): The field coefficient used in the computation of
+            homoology. Defaults to 2.
+        metric (str, optional): The metric used to compute distance between
+            data points. Must be one of the metrics listed in
+            ``sklearn.metrics.pairwise.PAIRWISE_DISTANCE_FUNCTIONS``.
+            Defaults to `"euclidean"`.
+        metric_params (dict, optional): Additional parameters to be passed to
+            the distance function. Defaults to None.
         verbose (bool, optional): Whether or not to display print some progress
             during fitting. Defaults to `False`.
 
@@ -44,21 +49,24 @@ class DripsComplex(BaseEstimator):
             of the homological generators in dimension i-1. In particular, the
             list starts with 0-dimensional homology and contains information
             from consecutive homological dimensions.
-
-    References:
-        #TODO
     """
 
     def __init__(
         self,
-        metric: str = "euclidean",
         max_dimension: int = 1,
         max_filtration: float = np.inf,
+        coeff: int = 2,
+        metric: str = "euclidean",
+        metric_params: Optional[dict] = None,
         verbose: bool = False,
     ) -> None:
-        self.metric = metric
         self.max_dimension = max_dimension
+        self.coeff = coeff
         self.max_filtration = max_filtration
+        self.metric = metric
+        if metric_params is None:
+            metric_params = dict()
+        self.metric_params = metric_params
         self.verbose = verbose
 
     def vprint(
@@ -71,51 +79,46 @@ class DripsComplex(BaseEstimator):
             pass
         return
 
-    def fit(
+    def fit_transform(
         self,
-        vertices: npt.NDArray,
-        witnesses: npt.NDArray,
-        n_threads: int = -1,
-        precision: str = "double",
+        X: list[npt.NDArray],
+        y: Optional[None] = None,
         swap: bool = False,
-        **persistence_kwargs,
+        n_threads: int = 1,
     ) -> Self:
         """Method that fits an `DripsComplex`-instance to a pair of point
-        clouds consisting of vertices and witnesses.
+        clouds consisting of vertices and witnesses and computes the persistent
+        homology of the associated Dowker-Rips complex.
 
         Args:
-            vertices (numpy.ndarray of shape (n_vertices, dim)): NumPy-array
-                containing the vertices.
-            witnesses (numpy.ndarray of shape (n_witnesses, dim)): NumPy-array
-                containing the witnesses.
+            X: List containing the NumPy-arrays of vertices and witnesses, in
+                this order.
+            y: Not used, present here for API consistency with scikit-learn.
+            swap: Whether or not to potentially swap the roles of vertices and
+                witnesses to compute the less expensive variant of persistent
+                homology. Defaults to `False`.
             n_threads (int, optional): Maximum number of threads to be used
                 during the computation in homology dimensions 1 and above. `-1`
                 means that the maximum number of threads will be used if
                 possible. Defaults to `1`.
-            precision (str, optional): Floating point precision to be used
-                throughout computation. Must be one of `"double"`, `"single"`
-                and `"half"`. Defaults to `"double"`
-            swap (bool, optional): Whether or not to potentially swap the roles
-                of vertices and witnesses to compute the less expensive variant
-                of persistent homology. Defaults to `False`.
 
         Returns:
             :class:`drips_complex.DripsComplex`: Fitted instance of
                 `DripsComplex`.
         """
-        self.precision_ = precision
-        match self.precision_:
-            case "double":
-                dtype = np.float64
-            case "single":
-                dtype = np.float32  # type: ignore
-            case "half":
-                dtype = np.float16  # type: ignore
+        vertices, witnesses = X
+        if vertices.shape[1] != witnesses.shape[1]:
+            raise ValueError(
+                "The vertices and witnesses should be of the same "
+                f"dimensionality; received dim(vertices)={vertices.shape[1]} "
+                f"and dim(witnesses)={witnesses.shape[1]}."
+            )
         self.swap_ = swap
         if self.swap_ and len(vertices) > len(witnesses):
             vertices, witnesses = witnesses, vertices
-        self.vertices_ = vertices.astype(dtype)
-        self.witnesses_ = witnesses.astype(dtype)
+            self.vprint("Swapped roles of vertices and witnesses.")
+        self.vertices_ = vertices
+        self.witnesses_ = witnesses
         self._labels_vertices_ = np.zeros(len(self.vertices_))
         self._labels_witnesses_ = -np.ones(len(self.witnesses_))
         self._points_ = np.concatenate([self.vertices_, self.witnesses_])
@@ -124,9 +127,7 @@ class DripsComplex(BaseEstimator):
             self._labels_witnesses_
         ])
         self.vprint("Getting ripser input...")
-        self._ripser_input_ = self._get_ripser_input(
-            dtype,
-        )
+        self._ripser_input_ = self._get_ripser_input()
         self.vprint("Done getting ripser input.")
         self.vprint("Computing persistent homology...")
         self.persistence_ = ripser_parallel(
@@ -135,23 +136,18 @@ class DripsComplex(BaseEstimator):
             maxdim=self.max_dimension,
             thresh=self.max_filtration,
             collapse_edges=True,
-            return_generators=False,
             n_threads=n_threads,
-            **persistence_kwargs,
         )["dgms"]
         self.vprint("Done computing persistent homology.")
-        return self
+        return self.persistence_
 
     def _get_ripser_input(
         self,
-        dtype,
     ):
-        # Vertex wgts: min dist to W
-        # Edge wgts: min_{w\in W}(max[d(v,w), d(v',w)])
         @jit(nopython=True, parallel=True)
         def _ripser_input_numba(dm):
             n = dm.shape[0]
-            ripser_input = np.empty((n, n), dtype=dtype)
+            ripser_input = np.empty((n, n), dtype=np.float32)
             for i in prange(n):
                 for j in range(i, n):
                     dist = np.min(np.maximum(dm[i], dm[j]))
@@ -159,7 +155,12 @@ class DripsComplex(BaseEstimator):
                     ripser_input[j, i] = dist
             return ripser_input
         return _ripser_input_numba(
-            pairwise_distances(self.vertices_, self.witnesses_)
+            pairwise_distances(
+                X=self.vertices_,
+                Y=self.witnesses_,
+                metric=self.metric,
+                **self.metric_params,
+            )
         )
 
     def plot_persistence(
@@ -179,7 +180,11 @@ class DripsComplex(BaseEstimator):
             :class:`plotly.graph_objs._figure.Figure`: A plot of the
                 persistence diagram.
         """
-        check_is_fitted(self, attributes="persistence_")
+        if not hasattr(self, "persistence_"):
+            raise AttributeError(
+                "This instance does not have the attribute `persistence_`. "
+                "Run `fit_transform` before plotting."
+            )
         fig = plot_persistences(
             [self.persistence_],
             **plotting_kwargs,
@@ -198,8 +203,8 @@ class DripsComplex(BaseEstimator):
         dimension three only.
 
         Args:
-            indicate_witnesses (bool, optional): Whether or not to use a
-                distinguished marker to indicate the witness points.
+            indicate_witnesses (bool, optional): Whether or not to indicate the
+                witness points by a cross as opposed to a dot.
                 Defaults to `True`.
             use_colors (bool, optional): Whether or not to color the vertices
                 and witnesses in different colors. Defaults to `True`.
@@ -211,7 +216,11 @@ class DripsComplex(BaseEstimator):
             :class:`plotly.graph_objs._figure.Figure`: A plot of the
                 vertex and witness point clouds.
         """
-        check_is_fitted(self, attributes=["vertices_", "witnesses_"])
+        if not hasattr(self, "persistence_"):
+            raise AttributeError(
+                "This instance does not have the attributes `vertices_` and "
+                "`witnesses_`. Run `fit_transform` before plotting."
+            )
         if self._points_.shape[1] not in {1, 2, 3}:
             raise Exception(
                 "Plotting is supported only for data "
@@ -225,12 +234,3 @@ class DripsComplex(BaseEstimator):
             colorscale="wong",
             **plotting_kwargs,
         )
-
-
-if __name__ == "__main__":
-    n = 100
-    ratio_vertices = 0.9
-    X = np.random.randn(n, 512)
-    V, W = X[:int(ratio_vertices * n)], X[int(ratio_vertices) * n:]
-    drc = DripsComplex().fit(V, W)
-    print("Success.")
