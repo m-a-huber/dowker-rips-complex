97152ee23058cc0652e06706f1614341160f64fd
m-a-huber <hubermarius@hotmail.com>
2025-02-12 09:57:38 +0100

    Reorder tests.

--- tests/test_drips_complex.py
+++ tests/test_drips_complex.py
@@ -5,29 +5,7 @@ from sklearn.model_selection import train_test_split  # type: ignore
 from drips_complex import DripsComplex  # type: ignore
 
 
-@pytest.fixture
-def data():
-    n, dim = 500, 512
-    ratio_vertices = 0.9
-    X, y = list(
-        train_test_split(
-            np.random.randn(n, dim),
-            train_size=ratio_vertices
-        )
-    ), None
-    return X, y
-
-
-def test_drips_complex(data):
-    """
-    Check whether `DripsComplex` runs at all and plots.
-    """
-    drc = DripsComplex()
-    drc.fit_transform(*data)
-    assert hasattr(drc, "persistence_")
-
-
-@pytest.fixture
+# @pytest.fixture
 def data_2_dim():
     n, dim = 500, 2
     ratio_vertices = 0.9
@@ -38,17 +16,6 @@ def data_2_dim():
         )
     ), None
     return X, y
-
-
-def test_drips_complex_plotting_2d(data_2_dim):
-    """
-    Check whether `DripsComplex` plots 2D data.
-    """
-    drc = DripsComplex()
-    drc.fit_transform(*data_2_dim)
-    assert hasattr(drc, "persistence_")
-    drc.plot_points()
-    drc.plot_persistence()
 
 
 @pytest.fixture
@@ -64,15 +31,17 @@ def data_3_dim():
     return X, y
 
 
-def test_drips_complex_plotting_3d(data_3_dim):
-    """
-    Check whether `DripsComplex` plots 3D data.
-    """
-    drc = DripsComplex()
-    drc.fit_transform(*data_3_dim)
-    assert hasattr(drc, "persistence_")
-    drc.plot_points()
-    drc.plot_persistence()
+@pytest.fixture
+def data_high_dim():
+    n, dim = 500, 512
+    ratio_vertices = 0.9
+    X, y = list(
+        train_test_split(
+            np.random.randn(n, dim),
+            train_size=ratio_vertices
+        )
+    ), None
+    return X, y
 
 
 @pytest.fixture
@@ -91,6 +60,56 @@ def quadrilateral():
     ])
     X, y = [vertices, witnesses], None
     return X, y
+
+
+@pytest.fixture
+def octagon():
+    t = 1 / np.sqrt(2)
+    vertices = np.array([
+        [1, 0],
+        [t, t],
+        [0, 1],
+        [-t, t]
+    ])
+    witnesses = np.array([
+        [-1, 0],
+        [-t, -t],
+        [0, -1],
+        [t, -t]
+    ])
+    X, y = [vertices, witnesses], None
+    return X, y
+
+
+def test_drips_complex(data_high_dim):
+    """
+    Check whether `DripsComplex` runs at all and plots.
+    """
+    drc = DripsComplex()
+    drc.fit_transform(*data_high_dim)
+    assert hasattr(drc, "persistence_")
+
+
+def test_drips_complex_plotting_2d(data_2_dim):
+    """
+    Check whether `DripsComplex` plots 2D data.
+    """
+    drc = DripsComplex()
+    drc.fit_transform(*data_2_dim)
+    assert hasattr(drc, "persistence_")
+    drc.plot_points()
+    drc.plot_persistence()
+
+
+def test_drips_complex_plotting_3d(data_3_dim):
+    """
+    Check whether `DripsComplex` plots 3D data.
+    """
+    drc = DripsComplex()
+    drc.fit_transform(*data_3_dim)
+    assert hasattr(drc, "persistence_")
+    drc.plot_points()
+    drc.plot_persistence()
 
 
 def test_drips_complex_quadrilateral(quadrilateral):
@@ -115,25 +134,6 @@ def test_drips_complex_quadrilateral(quadrilateral):
     ).all()
 
 
-@pytest.fixture
-def octagon():
-    t = 1 / np.sqrt(2)
-    vertices = np.array([
-        [1, 0],
-        [t, t],
-        [0, 1],
-        [-t, t]
-    ])
-    witnesses = np.array([
-        [-1, 0],
-        [-t, -t],
-        [0, -1],
-        [t, -t]
-    ])
-    X, y = [vertices, witnesses], None
-    return X, y
-
-
 def test_drips_complex_octagon(octagon):
     """
     Check whether `DripsComplex` returns correct result on regular octagon.
@@ -153,3 +153,6 @@ def test_drips_complex_octagon(octagon):
     assert (
         drc.persistence_[1] == np.empty(shape=(0, 2)).astype(np.float32)
     ).all()
+
+
+test_drips_complex_plotting_2d(data_2_dim())
